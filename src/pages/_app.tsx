ff4790e5d6fd14a6527a0a3c470bd0d80ceb202d
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-04-06 22:05:46 -0300

    Add ThemeProvider of Chrakra-ui and customize gray colors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
 import { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../styles/theme";
+
+//! Por padrão o Chakra vem com o CSSReset como true.
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
