0904ceda3f190e31e595e2bad5d6f53ab1e15143
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-04-21 21:18:15 -0300

    Change fetch for axios.

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -23,13 +23,13 @@ import { Pagination } from "../../components/Pagination";
 import { SideBar } from "../../components/SideBar";
 
 import Link from "next/link";
+import { api } from "../../services/api";
 
 export default function UserList() {
   const { data, isLoading, isFetching, error } = useQuery(
     "users",
     async () => {
-      const response = await fetch("http://localhost:3000/api/users");
-      const data = await response.json();
+      const { data } = await api.get("/users");
 
       const users = data.users.map((user) => {
         return {
